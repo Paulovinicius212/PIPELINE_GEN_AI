90a47746ff348d895c3941eb61a02ce50e665b59
PAULO VINICIUS <121190664+Paulovinicius212@users.noreply.github.com>
2024-09-11 22:19:08 -0300

    feat: adicionar SQL

--- database.py
+++ database.py
@@ -9,10 +9,11 @@ import os
 load_dotenv()
 
 # Configuração do banco de dados PostgreSQL
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("dpg-crh2pctsvqrc7387c970-a.oregon-postgres.render.com")
+DB_NAME = os.getenv("ciencia")
+DB_USER = os.getenv("ciencia_user")
+DB_PASS = os.getenv("L1mB0823UBGqgI3UfcsFmcurTWrlaQkp")
+
 
 # Função para salvar os dados validados no PostgreSQL
 def salvar_no_postgres(dados: Vendas):
@@ -21,24 +22,24 @@ def salvar_no_postgres(dados: Vendas):
     """
     try:
         conn = psycopg2.connect(
-            host=DB_HOST,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASS
+            host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS
         )
         cursor = conn.cursor()
-        
+
         # Inserção dos dados na tabela de vendas
         insert_query = sql.SQL(
             "INSERT INTO vendas (email, data, valor, quantidade, produto) VALUES (%s, %s, %s, %s, %s)"
         )
-        cursor.execute(insert_query, (
-            dados.email,
-            dados.data,
-            dados.valor,
-            dados.quantidade,
-            dados.produto.value
-        ))
+        cursor.execute(
+            insert_query,
+            (
+                dados.email,
+                dados.data,
+                dados.valor,
+                dados.quantidade,
+                dados.produto.value,
+            ),
+        )
         conn.commit()
         cursor.close()
         conn.close()
